e591be3c9d8fead155baebe53366d88af2b95e18
CameronCollingham <54521760+CameronCollingham@users.noreply.github.com>
2019-08-29 18:48:30 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Cameron Collingham, Keith Whitehead
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cameron Collingham, Keith Whitehead\n");
+
+  return 0;
+}
