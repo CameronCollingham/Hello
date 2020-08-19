1be072db4b46af829a7c3f9fe1fac0e06f9bb0c5
gagecrowell <gcrowell3@huskers.unl.edu>
2020-08-19 16:42:55 -0500

    Initial commit

--- hello.c
+++ hello.c
@@ -1,18 +1,16 @@
 /**
- * Author: Cameron Collingham, Keith Whitehead
- * Date: 2019/08/27
+ * Author: Gage Crowell
+ * Date: 2020/18/08
  *
  * A simple hello world program in C
  *
  */
-#include<stdlib.h>
-#include<stdio.h>
-
-//changes made were because of assignment requirements
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("Cameron Collingham, my major is computer science\n");
+  printf("Gage Crowell! \n");
 
   return 0;
 }
